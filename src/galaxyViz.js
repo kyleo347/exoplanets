90055701c6595a4e86188a5d660d2f451e60730b
Kyle <kyleoconnor347@gmail.com>
2018-09-18 14:42:48 -0700

    add zoom

--- src/galaxyViz.js
+++ src/galaxyViz.js
@@ -4,7 +4,18 @@ import * as d3 from "d3";
 export default class GalaxyViz extends Component {
     constructor() {
         super()
-        this.draw = this.draw.bind(this)
+        this.state = {
+            width : Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+            height : Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+            svg : null,
+        }
+        
+        this.draw = this.draw.bind(this);
+        this.zoomed = this.zoomed.bind(this);
+        this.zoom = d3.zoom()
+        .scaleExtent([1,10])
+        .on("zoom", this.zoomed);
+        
     }
 
 
@@ -35,32 +46,35 @@ export default class GalaxyViz extends Component {
         )
     }
 
+    zoomed() {
+        this.state.svg.attr("transform", d3.event.transform)
+    }
     
     draw(props) {
         if (!props || !props.planets || !props.planets.length) {
             return;
         }
         let spacetime = d3.select('.viz'); 
-        let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        let maxRadius = Math.min(width, height);
+        let maxRadius = Math.min(this.state.width, this.state.height);
         // Space
-        let svg = spacetime.select("svg g");
-        if (svg.empty()) {
-            svg = spacetime.append("svg")
-            .attr("width", width)
-            .attr("height", height)
+        this.state.svg = spacetime.select("svg g");
+        if (this.state.svg.empty()) {
+            this.state.svg = spacetime.append("svg")
+            .attr("width", this.state.width)
+            .attr("height", this.state.height)
+            .call(this.zoom)
+            .call(this.zoom.transform, d3.zoomIdentity.translate(this.state.width/2,this.state.height/2))
             .append("g")
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            .attr("transform", "translate(" + this.state.width / 2 + "," + this.state.height / 2 + ")");
         } 
-
+        //Create stars and move them to proper locations
         let max_dist = 0;
         let max_st_rad = 0
         this.props.planets.map(pl => {
             if (pl.st_dist > max_dist) {max_dist = pl.st_dist} 
             if (pl.st_rad > max_st_rad) {max_st_rad = pl.st_rad} 
         });
-        svg.selectAll('circle')
+        this.state.svg.selectAll('circle')
         .data(props.planets)
         .enter()
         .append("circle")
